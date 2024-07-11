a3e374c94390b5a1e22396b597d44dcf4e05381a
sumin <suminshin211@gmail.com>
2024-07-11 16:58:22 +0900

    댓글 페이지 라우팅 작업

--- src/myPage/pages/PostDetail.js
+++ src/myPage/pages/PostDetail.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Comment from '../components/Comment';
 import NewComment from '../components/NewComment';
 import './PostDetail.css';
+import MyTabs from "../components/myTabs";
 
 const PostDetail = ({ communities, addComment }) => {
+  const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate(`/`);
+    }
+    
   const { communityId, postId } = useParams();
   const community = communities.find(c => c.id === parseInt(communityId));
   const post = community?.posts.find(p => p.id === parseInt(postId));
@@ -12,9 +19,18 @@ const PostDetail = ({ communities, addComment }) => {
   if (!community || !post) {
     return <div>게시글을 찾을 수 없습니다.</div>;
   }
-
+  
   return (
-    <div className="post-detail">
+    <div>
+      <nav className="topNav">
+        <li className="Logo" onClick={goHome}>
+          <img className="imgLogo" src={require('../img/MainLogo.png')} alt="Logo" />
+        </li>
+        <li>
+          <MyTabs />
+        </li>
+      </nav>
+      <div className="post-detail">
       <h2>{post.title}</h2>
       <p>{post.content}</p>
       <h3>댓글</h3>
@@ -24,6 +40,7 @@ const PostDetail = ({ communities, addComment }) => {
         ))}
       </ul>
       <NewComment communityId={community.id} postId={post.id} addComment={addComment} />
+    </div>
     </div>
   );
 };
